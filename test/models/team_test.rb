79b2089a7e45778bdb7734f9941037489e7afabf
Ben Lerner <blerner@ccs.neu.edu>
2017-06-05 22:00:50 -0400

    Date and DateTime are not the same thing: apparently these tests are now failing for me (though they weren't, before)

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -12,20 +12,20 @@ class TeamTest < ActiveSupport::TestCase
     
   test "Dissolve team with nil end_date" do
     assert @team.end_date.nil?
-    @when = DateTime.now
+    @when = Date.today
     @team.dissolve(@when)
     assert_equal @when.to_date, @team.end_date
   end
   test "Dissolve team with future end_date" do
-    @when = DateTime.now
-    @team.end_date = @when + 1.day
+    @when = Date.today
+    @team.end_date = (@when + 1.day)
     assert !@team.end_date.nil?
     @team.dissolve(@when)
     assert_equal @when.to_date, @team.end_date
   end
   test "Dissolve team with expired end_date" do
-    @when = DateTime.now
-    @team.end_date = @when - 1.day
+    @when = Date.today
+    @team.end_date = (@when - 1.day)
     assert !@team.end_date.nil?
     @team.dissolve(@when)
     assert_equal @when.to_date - 1.day, @team.end_date
