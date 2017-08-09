9ee60533a63c13fdf439dacacad163364035a489
Nat Tuck <nat@ferrus.net>
2017-08-09 12:36:23 -0400

    Recreate delayed jobs table on migration down.

--- db/migrate/20170805001646_drop_delayed_jobs.rb
+++ db/migrate/20170805001646_drop_delayed_jobs.rb
@@ -4,6 +4,20 @@ class DropDelayedJobs < ActiveRecord::Migration[5.1]
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+	  create_table "delayed_jobs", force: :cascade do |t|
+	    t.integer  "priority",   default: 0, null: false
+	    t.integer  "attempts",   default: 0, null: false
+	    t.text     "handler",                null: false
+	    t.text     "last_error"
+	    t.datetime "run_at"
+	    t.datetime "locked_at"
+	    t.datetime "failed_at"
+	    t.string   "locked_by"
+	    t.string   "queue"
+	    t.datetime "created_at"
+	    t.datetime "updated_at"
+	  end
+
+	  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
   end
 end
