7df9709c38b22616a5d498c769815466c46005b8
Ben Lerner <blerner@ccs.neu.edu>
2017-10-10 12:43:07 -0400

    cleanup error messages to include names of questions when possible

--- app/models/assignments/codereview.rb
+++ app/models/assignments/codereview.rb
@@ -103,109 +103,115 @@ class Codereview < Assignment
           next
         else
           section.each do |secName, sec_questions|
-            sec_questions.each do |question|
+            sec_questions.each_with_index do |question, q_index|
               question.each do |type, q|
                 question_count += 1
+                question_desc =
+                  if question.dig(type, "name")
+                    "\"#{question[type]["name"]}\" (question #{q_index + 1} within section #{secName})"
+                  else
+                    "Question #{question_count} (in section #{secName})"
+                  end
                 begin
                   if !(type.is_a? String)
-                    self.errors.add(:base, "Question #{question_count} (in section #{secName}) has unknown type #{type}")
+                    self.errors.add(:base, "#{question_desc} has unknown type #{type}")
                     next
                   elsif !question_kinds.member?(type.underscore)
-                    self.errors.add(:base, "Question #{question_count} (in section #{secName}) has unknown type #{type}")
+                    self.errors.add(:base, "#{question_desc} has unknown type #{type}")
                     next
                   else
                     if q["weight"].nil? || !is_float(q["weight"])
-                      self.errors.add(:base, "Question #{question_count} has missing or invalid weight")
+                      self.errors.add(:base, "#{question_desc} has missing or invalid weight")
                     end
                     @total_weight += Float(q["weight"])
                     ans = q["correctAnswer"]
                     if ans.nil?
-                      self.errors.add(:base, "Question #{question_count} is missing a correctAnswer")
+                      debugger
+                      self.errors.add(:base, "#{question_desc} is missing a correctAnswer")
                     end
                     if q["rubric"].nil?
-                      self.errors.add(:base, "Question #{question_count} is missing a rubric")
+                      self.errors.add(:base, "#{question_desc} is missing a rubric")
                     elsif !(q["rubric"].is_a? Array)
-                      self.errors.add(:base, "Question #{question_count} has an invalid rubric")
+                      self.errors.add(:base, "#{question_desc} has an invalid rubric")
                     else
                       q["rubric"].each_with_index do |guide, i|
                         if !(guide.is_a? Object) || guide.keys.count != 1
-                          self.errors.add(:base, "Question #{question_count}, rubric entry #{i} is ill-formed")
+                          self.errors.add(:base, "#{question_desc}, rubric entry #{i} is ill-formed")
                         else
                           guide.each do |weight, desc|
                             if !is_float(weight)
-                              self.errors.add(:base, "Question #{question_count}, rubric entry #{i} has non-numeric weight")
+                              self.errors.add(:base, "#{question_desc}, rubric entry #{i} has non-numeric weight")
                             elsif Float(weight) < 0 || Float(weight) > 1
-                              self.errors.add(:base, "Question #{question_count}, rubric entry #{i} has out-of-bounds weight")
+                              self.errors.add(:base, "#{question_desc}, rubric entry #{i} has out-of-bounds weight")
                             end
                             if desc.is_a? String
                               # ok
                             elsif (desc.is_a? Object) && !((desc["hint"].is_a? String) || (desc["feedback"].is_a? String))
-                              self.errors.add(:base, "Question #{question_count}, rubric entry #{i} has malformed feedback")
+                              self.errors.add(:base, "#{question_desc}, rubric entry #{i} has malformed feedback")
                             end
                           end
                         end
                       end
                     end
                     if q["prompt"].nil?
-                      self.errors.add(:base, "Question #{question_count} is missing a prompt")
+                      self.errors.add(:base, "#{question_desc} is missing a prompt")
                     end
                     case type
                     when "YesNo", "TrueFalse"
                       if ![true, false].member?(q["correctAnswer"])
-                        self.errors.add(:base, "Boolean question #{question_count} has a non-boolean correctAnswer")
+                        self.errors.add(:base, "Boolean question #{question_desc} has a non-boolean correctAnswer")
                       end
                     when "Numeric"
                       min = q["min"]
                       max = q["max"]
                       if max.nil? || !is_float(min)
-                        self.errors.add(:base, "Numeric question #{question_count} has a non-numeric max")
+                        self.errors.add(:base, "Numeric question #{question_desc} has a non-numeric max")
                       else
                         max = max.to_f
                       end
                       if min.nil? || !is_float(min)
-                        self.errors.add(:base, "Numeric question #{question_count} has a non-numeric min")
+                        self.errors.add(:base, "Numeric question #{question_desc} has a non-numeric min")
                       else
                         min = min.to_f
                       end
                       if ans.nil? || !is_float(ans)
-                        self.errors.add(:base, "Numeric question #{question_count} has a non-numeric ans")
+                        self.errors.add(:base, "Numeric question #{question_desc} has a non-numeric ans")
                       else
                         ans = ans.to_f
                       end
                       if is_float(min) && is_float(max) && is_float(ans) && !(min <= ans and ans <= max)
-                        self.errors.add(:base, "Numeric question #{question_count} has a correctAnswer outside the specified range")
+                        self.errors.add(:base, "Numeric question #{question_desc} has a correctAnswer outside the specified range")
                       end
                     when "MultipleChoice"
-                      if q["options"].nil? || !(q["options"].is_a? Array)
-                        self.errors.add(:base, "MultipleChoice question #{question_count} is missing an array of choices")
-                      end
                       if !is_int(ans)
-                        self.errors.add(:base, "MultipleChoice question #{question_count} has a non-numeric correctAnswer")
+                        self.errors.add(:base, "MultipleChoice question #{question_desc} has a non-numeric correctAnswer")
                       else
                         ans = ans.to_i
                       end
-                      if is_int(ans) && (ans < 0 || ans >= q["options"].count)
-                        self.errors.add(:base, "MultipleChoice question #{question_count} has a correctAnswer not in the available choices")
+                      if q["options"].nil? || !(q["options"].is_a? Array)
+                        self.errors.add(:base, "MultipleChoice question #{question_desc} is missing an array of choices")
+                      elsif is_int(ans) && (ans < 0 || ans >= q["options"].count)
+                        self.errors.add(:base, "MultipleChoice question #{question_desc} has a correctAnswer not in the available choices")
                       end
                     end
                     if q["parts"]
                       if !q["parts"].is_a? Array
-                        self.errors.add(:base, "Question #{question_count} has a non-list of parts")
+                        self.errors.add(:base, "#{question_desc} has a non-list of parts")
                       else
                         q["parts"].each_with_index do |part, part_i|
                           if !part.is_a? Object
-                            self.errors.add(:base, "Question #{question_count} has a non-object part ##{part_i + 1}")
+                            self.errors.add(:base, "#{question_desc} has a non-object part ##{part_i + 1}")
                           elsif part.keys.count > 1
-                            self.errors.add(:base, "Question #{question_count} part ##{part_i + 1} has too many keys")
+                            self.errors.add(:base, "#{question_desc} part ##{part_i + 1} has too many keys")
                           elsif !["codeTag", "codeTags", "requiredText", "text"].member?(part.keys[0])
-                            self.errors.add(:base, "Question #{question_count} part ##{part_i + 1} has an invalid type #{part.keys[0]}")
+                            self.errors.add(:base, "#{question_desc} part ##{part_i + 1} has an invalid type #{part.keys[0]}")
                           end
                         end
                       end
                     end
                   end
                 rescue Exception => e
-                  self.errors.add(:base, "Question #{question_count} in section #{secName} could not be parsed: #{e}")
+                  self.errors.add(:base, "#{question_desc} could not be parsed: #{e}")
                 end
               end
             end
