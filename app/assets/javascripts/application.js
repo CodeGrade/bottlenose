0e98555d4d4aab44a01334e792b48a6ce4c7e9e8
Ben Lerner <blerner@ccs.neu.edu>
2017-11-28 21:55:09 -0500

    Improvements to file rendering:
    * Render XML, HTML and SVG files both with syntax highlighting and within a sandboxed iframe.  Scripts are disabled, and a clickjacking div is placed over the iframe to prevent clicking on links.  This should completely immobilize the page, and prevent security issues (since without scripts or links, the page can't exfiltrate sensitive data anywhere).
    * If a file is more than 10KLOC long, don't try to display it in CodeMirror -- it'll choke the browser

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,9 @@
 //= require codemirror/mode/javascript/javascript
 //= require codemirror/mode/markdown/markdown
 //= require codemirror/mode/scheme/scheme
+//= require codemirror/mode/css/css
+//= require codemirror/mode/xml/xml
+//= require codemirror/mode/htmlmixed/htmlmixed
 //= require pyret-codemirror-mode/mode/pyret
 //= require_tree .
 
