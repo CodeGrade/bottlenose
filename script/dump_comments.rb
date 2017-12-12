3241d064f13a732eb764a681215880eecd5ed56f
Ben Lerner <blerner@ccs.neu.edu>
2017-12-12 17:57:21 -0500

    useful fixes

--- script/dump_comments.rb
+++ script/dump_comments.rb
@@ -1,12 +1,23 @@
 require 'active_record'
 
+APP_PATH = File.expand_path('../../config/application',  __FILE__)
+require File.expand_path('../../config/boot',  __FILE__)
+require APP_PATH
+Rails.application.require_environment!
+
 InlineComment
   .joins(:submission).where("submissions.assignment_id": ARGV[0].to_i)
-  .joins(:grader).where("graders.grader_config_id": ARGV[1].to_i)
+  .joins(:grade).where("grades.grader_id": ARGV[1].to_i)
   .group_by(&:submission)
   .each do |sub, cs|
+  if ARGV[2]
+    match = ARGV[2].downcase
+    cs = cs.keep_if{|c| c.comment.downcase.include?(match)}
+  end
+  next if cs.empty?
+  print "#{sub.team&.to_s || sub.user.name} -- Sub ##{sub.id}:\n"
   cs.each do |c|
-    print "#{c.user.name} -- #{c.submission_id}, #{c.filename.gsub(Regexp.new('.*extracted/?'), '')}, #{c.line}: #{c.label}, #{c.severity} [#{c.weight}]: #{c.comment}\n"
+    print "#{c.filename.gsub(Regexp.new('.*extracted/?'), '')}, #{c.line}: #{c.label}, #{c.severity} [#{c.weight}]: #{c.comment}\n"
   end
   print "==================================================\n\n"
 end
