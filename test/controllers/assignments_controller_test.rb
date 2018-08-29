9d585faf856ea33d8c105f1b63305caa321580b3
Matt Singer <singerm144@gmail.com>
2018-08-29 18:09:22 -0400

    I said assertive, Spongebob

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -553,7 +553,10 @@ class AssignmentsControllerTest < ActionController::TestCase
     end
 
     @assn = assigns(:assignment)
-    assert_equal @assn, @assn.graders.first.upload.assignment
+    upload = @assn.graders.first.upload
+    assert_equal @assn, upload.assignment
+    assert upload.upload_dir.to_s.starts_with?(Upload.base_upload_dir.join(@cs101.id.to_s, @assn.id.to_s).to_s)
+    assert File.exists?(upload.submission_path)
     assert_redirected_to [@cs101, @assn.becomes(Assignment)]
   end
 
