2c95f350ef203bef70e3f92ee4b335fd28e5a926
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2018-09-25 12:24:36 -0400

    update string representation for check_section_toggles interlock

--- app/models/interlock.rb
+++ app/models/interlock.rb
@@ -6,12 +6,16 @@ class Interlock < ApplicationRecord
                     :check_section_toggles]
 
   def to_s
-    "#{Interlock.constraint_to_s(self.constraint)} #{self.related_assignment.name}"
+    if self.constraint == :check_section_toggles
+      Interlock.constraint_to_s(self.constraint)
+    else
+      "#{Interlock.constraint_to_s(self.constraint)} #{self.related_assignment.name}"
+    end
   end
   
   def self.constraint_to_s(con)
     {no_submission_unless_submitted: "Prohibit submission unless submitted to",
      no_submission_after_viewing: "Submissions are prohibited after viewing",
-     check_section_toggles: "Prohibit submission by section to"}[con.to_sym]
+     check_section_toggles: "Submissions are locked down by section"}[con.to_sym]
   end
 end
