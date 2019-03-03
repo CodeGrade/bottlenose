83a1d4482722f56c80f876008ccfccb908f1ed79
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2019-03-03 18:27:19 -0500

    add index to registrations for course-user combos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_011746) do
+ActiveRecord::Schema.define(version: 2019_03_03_232336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_011746) do
     t.string "tags", default: ""
     t.integer "role", default: 0, null: false
     t.datetime "dropped_date"
+    t.index ["course_id", "user_id"], name: "index_registrations_on_course_id_and_user_id", unique: true
     t.index ["course_id"], name: "index_registrations_on_course_id"
     t.index ["user_id"], name: "index_registrations_on_user_id"
   end
