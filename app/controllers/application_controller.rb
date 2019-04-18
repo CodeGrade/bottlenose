17edd50cf8a6b8c33636d45c08e607e78a1aee0b
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2019-04-18 05:14:22 -0400

    show LDAP error message in flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   impersonates :user
 
   rescue_from DeviseLdapAuthenticatable::LdapException, Net::LDAP::Error do |exception|
-    redirect_back fallback_location: root_path, alert: "There was an error logging in. Please contact a professor or site admin."
+    redirect_back fallback_location: root_path, alert: "There was an error logging in. Please contact a professor or site admin, and provide them the following information:<br>#{exception}"
   end
   protect_from_forgery
 
