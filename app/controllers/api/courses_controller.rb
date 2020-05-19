0fad37095c71c3e82cd4868a24ca0b065376f034
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-19 16:56:49 -0400

    start courses API

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,15 +1,47 @@
 module Api
   class CoursesController < ApiController
+    before_action -> { find_course(params[:id]) }, only: [:show]
+    before_action :require_admin_or_prof, only: [:show]
+    before_action :require_admin_or_prof_ever, only: [:index]
+
     def show
-      render json: {
-        this: "show",
-        you: current_api_user.username,
-        course: Course.find(params[:id]).name
-      }
+      # {
+      #   sections: [],
+      #   staff: [{ username, role }],
+      #   students: [user],
+      #   registrations: {
+      #     [username]: [sectionId]
+      #   },
+      # }
+      render json: serialize_course(@course)
     end
 
     def index
-      render json: { this: "index", you: current_api_user.username }
+      # [
+      #   {
+      #     term: { ... },
+      #     courses: [ { ... } ],
+      #   }
+      # ] sorted chronologically
+      render json: serialize_active_courses(current_user.active_courses)
+    end
+
+    private
+
+    def serialize_active_courses(active_courses)
+      active_courses.map do |sem, courses|
+        {
+          term: sem,
+          courses: courses.map { |c| serialize_course(c) }
+        }
+      end
+    end
+
+    def serialize_course(course)
+      {
+        id: course.id,
+        name: course.name
+      }
     end
   end
 end
