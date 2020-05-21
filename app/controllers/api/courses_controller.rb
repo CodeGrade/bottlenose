1998987474998eb57cb22348fba70b96608dbe4f
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-05-21 16:43:09 -0400

    show user role for course

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -30,7 +30,8 @@ module Api
     def serialize_course(course)
       {
         id: course.id,
-        name: course.name
+        name: course.name,
+        role: current_user.registration_for(course).role
         # students: course.students.map { |s| serialize_student(s) },
         # staff: course.sorted_staff.group_by(&:role).map { |k, v| [Registration::roles.invert[k], v.map { |u| serialize_user(u) }] }.to_h,
         # sections: course.sections.map { |s| serialize_section(s) },
