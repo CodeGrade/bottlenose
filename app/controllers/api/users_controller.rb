3e1183c8c75a4d8d400c4377b40cd5a4ffdb1e1e
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-14 14:41:12 -0400

    remove extra actions from api/users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,7 @@
 module Api
   class UsersController < ApiController
-    before_action :require_admin, except: [:me]
-
     def me
       render json: serialize_user(current_user)
-    end
-
-    def show
-      u = User.find_by(username: params[:id])
-      return head :not_found unless u
-
-      render json: serialize_user(u)
     end
   end
 end
