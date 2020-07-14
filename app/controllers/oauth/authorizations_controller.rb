5b9f2553353cf92ed6172d6357ff103bf8ebaf11
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-07-14 14:56:02 -0400

    impersonation comments

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -1,4 +1,6 @@
 module Oauth
   class AuthorizationsController < Doorkeeper::AuthorizationsController
+    # Note: Does NOT allow impersonating users so that logging into Hourglass (or other OAuth apps) is disjoint from
+    # Bottlenose's impersonation status
   end
 end
