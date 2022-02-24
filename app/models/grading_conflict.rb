60a27e2e4d8318de4f465883848961e433c9c848
williams-jack <williams.jack@northeastern.edu>
2022-02-24 13:23:48 -0500

    Added course role validation to grading conflict. Added proper migration and schema update.

--- app/models/grading_conflict.rb
+++ app/models/grading_conflict.rb
@@ -1,10 +1,19 @@
 class GradingConflict < ApplicationRecord
 
-  # TODO: Need to add validations to ensure grader_user is a grader (check with course)
-  # TODO: Need to add validation to ensure gradee_user is a student (check role with course)
-  # TODO: What else needs to be validated?
-
-  belongs_to :grader_user
-  belongs_to :gradee_user
+  belongs_to :grader_user, class_name: "User"
+  belongs_to :gradee_user, class_name: "User"
   belongs_to :course
+
+   # TODO: What else needs to be validated?
+  validate :users_have_proper_roles
+
+
+  # N/A -> Boolean 
+  # Ensures that the :grader_user is a grader and the :gradee_user 
+  # is a normal student
+  def users_have_proper_roles
+    return (self.grader_user.course_grader?(course) || 
+      self.grader_user.course_assistant?(course)) &&
+      self.gradee_user.course_student?(course)
+  end
 end
