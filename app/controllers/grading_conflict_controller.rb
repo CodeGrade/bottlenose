17bd3a1b377eb119a0e5888ea117e19d7467fcb3
williams-jack <williams.jack@northeastern.edu>
2022-02-24 15:52:58 -0500

    Added basic scaffolding for GradingConflict controller.

--- app/controllers/grading_conflict_controller.rb
+++ app/controllers/grading_conflict_controller.rb
@@ -1,2 +1,28 @@
 class GradingConflictController < ApplicationController
+
+    # TODO: Add validation to confirm creation can only be done by a:
+    #   - Grader in the course
+    #   - Professor in the course
+    #   - Admin (regardless of role in the course)
+    
+    # TODO: Add validation to confirm editing/deleting of conflict can only be 
+    # done by a:
+    #   - Professor in the course
+    #   - Admin (regardless of role in the course)
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def delete
+    end
+
+    def update
+    end
+    
 end
