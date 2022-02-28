316ddc29b976d32032608de8cc7d820864ae13a5
williams-jack <williams.jack@northeastern.edu>
2022-02-27 21:08:15 -0500

    added validation to grading conflict controller.

--- app/controllers/grading_conflict_controller.rb
+++ app/controllers/grading_conflict_controller.rb
@@ -1,20 +1,14 @@
 class GradingConflictController < ApplicationController
 
-    # TODO: Add validation to confirm creation can only be done by a:
-    #   - Grader in the course
-    #   - Professor in the course
-    #   - Admin (regardless of role in the course)
-    
-    # TODO: Add validation to confirm editing/deleting of conflict can only be 
-    # done by a:
-    #   - Professor in the course
-    #   - Admin (regardless of role in the course)
+    before_action :require_admin_or_staff
+    before_action :require_admin_or_prof, only: [:delete, :update]
 
     def index
     end
 
-    def show
-    end
+    # TODO: Should there be a show method? May not be enough info/data
+    # to warrant showing an individual one since it's essentially
+    # just a pair of usernames.
 
     def create
     end
@@ -24,5 +18,9 @@ class GradingConflictController < ApplicationController
 
     def update
     end
+
+    private
+
+    # TODO: Add param methods
     
 end
