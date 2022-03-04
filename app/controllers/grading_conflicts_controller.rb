a72552da32bf3618f685b859bd20ad77553a8c4d
williams-jack <williams.jack@northeastern.edu>
2022-03-03 22:33:10 -0500

    Updated table on GC index page. Began work on new GC form.

--- app/controllers/grading_conflicts_controller.rb
+++ app/controllers/grading_conflicts_controller.rb
@@ -7,6 +7,7 @@ class GradingConflictsController < ApplicationController
     before_action :require_admin_or_prof, only: [:delete, :update]
 
     def index
+        
         if current_user.course_professor?(@course) || current_user.site_admin
             @grading_conflicts = GradingConflict.where(course: @course)
         elsif current_user.course_assistant?(@course)
@@ -14,6 +15,11 @@ class GradingConflictsController < ApplicationController
         else
             @grading_conflicts = GradingConflict.where(course: @course, student: current_user)
         end
+
+        # @active_and_inactive_conflicts = grading_conflicts.where(status: :inactive)
+        #     .or(grading_conflicts.where(status: :active))
+        # @pending_conflicts = grading_conflicts.where(status: :pending)
+
     end
 
     # TODO: Should there be a show method? May not be enough info/data
@@ -24,6 +30,12 @@ class GradingConflictsController < ApplicationController
     end
 
     def new
+        @tas_and_graders = Registration.where(course: @course)
+            .where(role: Registration.roles[:grader])
+            .or(Registration.where(role: Registration.roles[:assistant]))
+            .map{|reg| reg.user}
+        @students = Registration.where(course: @course, role: Registration.roles[:student])
+            .map{|reg| reg.user}
     end
 
     def create
