79466d869e84cb90c3fa23f8b2b4be945b7ec2c4
williams-jack <williams.jack@northeastern.edu>
2022-03-23 17:05:22 -0400

    Added table sorting to single conflict's audit log. Fixed resubmitting a conflict request.

--- app/models/grading_conflict_audit.rb
+++ app/models/grading_conflict_audit.rb
@@ -2,4 +2,9 @@ class GradingConflictAudit < ApplicationRecord
   enum status: [:active, :inactive, :pending, :rejected]
   belongs_to :user
   belongs_to :grading_conflict
+
+  def reason=(value)
+    super(ActionController::Base.helpers.sanitize(value))
+  end
+
 end
