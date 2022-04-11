d421b04843043b455ddf8efc5163331d55c04718
Kyle Sferrazza <git@kylesferrazza.com>
2022-04-11 16:52:13 -0400

    tell selenium where to find chrome

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,8 @@
+require 'mkmf'
+
 Rails.application.configure do
+  Selenium::WebDriver::Chrome.path = find_executable 'chrome'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
