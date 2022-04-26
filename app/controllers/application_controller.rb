01bf81099e0f8c36d22aeb119af62e4a61aaae0f
Kyle Sferrazza <git@kylesferrazza.com>
2022-04-26 13:13:34 -0400

    load 5.2 defaults

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   rescue_from DeviseLdapAuthenticatable::LdapException, Net::LDAP::Error do |exception|
     redirect_back fallback_location: root_path, alert: "There was an error logging in. Please contact a professor or site admin, and provide them the following information:<br>#{exception}"
   end
-  protect_from_forgery
 
   if Rails.env.production?
     rescue_from ActionView::MissingTemplate do |exception|
