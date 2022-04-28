52188a7d6f5ceb97631656dee487fa368c049d9a
Kyle Sferrazza <git@kylesferrazza.com>
2022-04-28 12:24:10 -0400

    very WIP attempt at webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,67 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import 'tablesorter';
+import 'tablesorter/dist/css/theme.bootstrap_2.min.css'
+
+import 'jquery-ujs';
+
+import 'bootstrap-toggle';
+
+import '@nathanvda/cocoon';
+
+import 'codemirror/lib/codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/mdn-like.css';
+import 'codemirror/addon/runmode/runmode';
+import 'codemirror/addon/selection/active-line';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/mllike/mllike';
+import 'codemirror/mode/haskell/haskell';
+import 'codemirror/mode/haskell-literate/haskell-literate';
+import 'codemirror/mode/ebnf/ebnf';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/scheme/scheme';
+import 'codemirror/mode/commonlisp/commonlisp';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/stex/stex';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/yaml/yaml';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+
+
+$(function() {
+
+// require('../src/pdf_render');
+
+require('jquery-ui/ui/widgets/tooltip');
+require('../src/0startup');
+require('../src/application');
+require('../src/assignments')
+require('../src/bootstrap.treeview');
+require('../src/courses');
+require('../src/form-tabs');
+require('../src/grades');
+require('../src/jquery.matchHeight');
+require('../src/jquery.keyDecoder');
+require('../src/makefile-mode');
+require('../src/submissions');
+require('../src/teamsets');
+
+})
+
+// import '../src/pdf_render';
+// import '../src/0startup';
+// import '../src/application';
+// import '../src/assignments';
+// import '../src/bootstrap.treeview';
+// import '../src/courses';
+// import '../src/form-tabs';
+// import '../src/grades';
+// import '../src/jquery.matchHeight';
+// import '../src/jquery.keyDecoder';
+// import '../src/makefile-mode';
+// import '../src/submissions';
+// import '../src/teamsets';
--- app/javascript/src/makefile-mode.js
+++ app/javascript/src/makefile-mode.js
@@ -2,15 +2,9 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
+
+import CodeMirror from 'codemirror';
 
 CodeMirror.defineMode('makefile', function() {
 
@@ -141,5 +135,3 @@ CodeMirror.defineMode('makefile', function() {
 });
 
 CodeMirror.defineMIME('text/x-makefile', 'makefile');
-
-});
