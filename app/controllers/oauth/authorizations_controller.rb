20ee96da3df32f9e315d61e18fc69abf793d9386
Ben Lerner <blerner@ccs.neu.edu>
2023-01-10 12:18:22 -0500

    HACK: Doorkeeper 5.4.0 + Rails 7 = Redirection errors when trying to log in.
        # NOTE: In Rails 7, redirect_to will throw an error if we don't allow_other_host.
        # Unfortunately, Doorkeeper 5.4.0 doesn't supply that option right now, but upgrading
        # to new versions seems to be backwards incompatible for some users(?)
        # For now, monkey-patch this call of redirect_to to add in the necessary flag.
        # Once we successfully upgrade Doorkeeper, we can remove this patch.

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -2,5 +2,15 @@ module Oauth
   class AuthorizationsController < Doorkeeper::AuthorizationsController
     # Note: Does NOT allow impersonating users so that logging into Hourglass (or other OAuth apps) is disjoint from
     # Bottlenose's impersonation status
+
+    # NOTE: In Rails 7, redirect_to will throw an error if we don't allow_other_host.
+    # Unfortunately, Doorkeeper 5.4.0 doesn't supply that option right now, but upgrading
+    # to new versions seems to be backwards incompatible for some users(?)
+    # For now, monkey-patch this call of redirect_to to add in the necessary flag.
+    # Once we successfully upgrade Doorkeeper, we can remove this patch.
+    def redirect_to(options = {}, response_options = {})
+      response_options[:allow_other_host] = true;
+      super(options, response_options)
+    end
   end
 end
