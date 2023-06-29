2022e1407b8f38f185517bdc67c30c9afe30b0c2
williams-jack <williams.jack@northeastern.edu>
2023-06-28 20:51:08 -0400

    feat: ability to fetch files from lib/assets
    This is implemented by adding a new resource method
    that maps to /resources/<file_path>.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,14 +1,10 @@
 class FilesController < ApplicationController
+
+  @@resources_true_dir = "lib/assets"
+
   def upload
     if File.file?(Upload.base_upload_dir.join(params[:path]))
-      case File.extname(params[:path]).downcase
-      when ".jpg", ".jpeg", ".png", ".gif", ".tap", ".log"
-        disp = "inline"
-      when ".pdf"
-        disp = nil
-      else
-        disp = "attachment"
-      end
+      disp = get_file_disposition(File.extname(params[:path]).downcase)
       mime = ApplicationHelper.mime_type(params[:path])
       
       send_file Upload.base_upload_dir.join(params[:path]).to_s,
@@ -19,4 +15,33 @@ class FilesController < ApplicationController
       render 'errors/not_found', layout: 'errors', formats: [:html], status: 404
     end
   end
+
+  def resource
+    if File.file?(Pathname.new(@@resources_true_dir).join(params[:path]))
+      disp = get_file_disposition(File.extname(params[:path]).downcase)
+      mime = ApplicationHelper.mime_type(params[:path])
+
+      send_file Pathname.new(@@resources_true_dir).join(params[:path]).to_s,
+                filename: File.basename(params[:path]),
+                disposition: disp,
+                type: mime
+    else
+      render 'errors/not_found', layout: 'errors', formats: [:html], status: 404
+    end
+  end
+
+
+  private
+  
+  def get_file_disposition(file_ext)
+    case file_ext
+    when ".jpg", ".jpeg", ".png", ".gif", ".tap", ".log"
+      return "inline"
+    when ".pdf"
+      return nil
+    else
+      return "attachment"
+    end
+  end
+
 end
