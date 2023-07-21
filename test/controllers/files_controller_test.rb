90012de1afa60680f2655c0a749b95549009abfa
williams-jack <williams.jack@northeastern.edu>
2023-07-20 23:18:21 -0400

    test: file paths and validation for FilesController

---
+++ test/controllers/files_controller_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+require 'tempfile'
+require 'fileutils'
+
+class FilesControllerTest < ActionController::TestCase
+
+  setup do
+    Upload.base_upload_dir.mkpath
+  end
+
+  teardown do
+    FileUtils.rmdir(Upload.base_upload_dir)
+  end
+
+  test "should get valid resource file path" do
+    Tempfile.create("foo", Rails.root.join("lib", "assets")) do |f|
+      file_name = Pathname.new(f.path).basename
+      get "resource", params: {
+        path: file_name
+      }
+      assert_response :success
+    end
+  end
+
+  test "should error on non existent resource file path" do
+    get "resource", params: {
+      path: "nonexistent_file.txt"
+    }
+    assert_response :missing
+  end
+
+  test "should error on resource path containing ../" do
+    # This is one directory above where the resources endpoint
+    # allows files to be sources from (i.e., lib/assets).
+    Tempfile.create("foo", Rails.root.join("lib")) do |f|
+      file_name = Pathname.new(f.path).basename
+      get "resource", params: {
+        path: "../#{file_name}"
+      }
+      assert_response :missing
+    end
+  end
+
+  test "should get valid upload path" do
+    Tempfile.create("foo", Upload.base_upload_dir) do |f|
+      file_name = Pathname.new(f.path).basename
+      get "upload", params: {
+        path: "#{file_name}"
+      }
+      assert_response :success
+    end
+  end
+
+  test "should error on non-existent upload path" do
+    get "upload", params: {
+      path: "non-existent.txt"
+    }
+    assert_response :missing
+  end
+
+  test "should error on upload path containing ../" do
+    Tempfile.create("foo", Upload.base_upload_dir.join("../")) do |f|
+      file_name = Pathname.new(f.path).basename
+      get "upload", params: {
+        path: "../#{file_name}"
+      }
+      assert_response :missing
+    end
+  end
+
+end
