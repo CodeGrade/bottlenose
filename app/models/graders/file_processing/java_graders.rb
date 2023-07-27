12be1b31d94777d8edb6d0248501a8fc75cd11fa
williams-jack <williams.jack@northeastern.edu>
2023-07-26 21:32:20 -0400

    chore: rename grader_path to grader_dir

--- app/models/graders/file_processing/java_graders.rb
+++ app/models/graders/file_processing/java_graders.rb
@@ -3,9 +3,9 @@ require 'fileutils'
 class JavaGraderFileProcessor
 
   def self.process_zip(upload, grader)
-    grader_path = upload.grader_path(grader)
-    starter_zip_path, testing_zip_path = grader_path.join("starter.zip"),
-                                          grader_path.join("testing.zip")
+    grader_dir = upload.grader_dir(grader)
+    starter_zip_path, testing_zip_path = grader_dir.join("starter.zip"),
+                                          grader_dir.join("testing.zip")
     # Since this method is run on a grader's :after_save hook, it's
     # possible files may already exist and need to be cleaned up.
     [starter_zip_path, testing_zip_path].each do |zip_path|
@@ -14,12 +14,12 @@ class JavaGraderFileProcessor
       end
     end
     include_starter_zip = false
-    if Dir.exists?(grader_path.join("starter")) && Dir.exists?(grader_path.join("testing"))
-      self.populate_zip(starter_zip_path, grader_path, "starter")
-      self.populate_zip(testing_zip_path, grader_path, "testing")
+    if Dir.exists?(grader_dir.join("starter")) && Dir.exists?(grader_dir.join("testing"))
+      self.populate_zip(starter_zip_path, grader_dir, "starter")
+      self.populate_zip(testing_zip_path, grader_dir, "testing")
       include_starter_zip = true
     else
-      self.populate_zip(testing_zip_path, grader_path, "")
+      self.populate_zip(testing_zip_path, grader_dir, "")
     end
     
     zip_file_paths = { testing: testing_zip_path }
@@ -28,12 +28,12 @@ class JavaGraderFileProcessor
     end
   end
 
-  def self.populate_zip(zip_file_path, grader_path, grader_contents_path)
+  def self.populate_zip(zip_file_path, grader_dir, grader_contents_path)
     Zip::File.open(zip_file_path, Zip::File::CREATE) do |zipfile|
-      Dir[File.join(grader_path, grader_contents_path, "**", "**")] do |file|
+      Dir[File.join(grader_dir, grader_contents_path, "**", "**")] do |file|
         path = Pathname.new(file)
         zipfile.add(
-          path.relative_path_from(Pathname.join(grader_path, grader_contents_path)),
+          path.relative_path_from(Pathname.join(grader_dir, grader_contents_path)),
           file
         )
       end
